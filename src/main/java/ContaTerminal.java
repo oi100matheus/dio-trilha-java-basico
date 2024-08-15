0f29a8ad46a3d8adc0d848214be9b266685c6752
Matheus Rangel <matheusrangel2003@hotmail.com>
2024-08-15 18:18:53 -0300

    meu-projeto

--- src/main/java/ContaTerminal.java
+++ src/main/java/ContaTerminal.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int numero;
+        String agencia;
+        String nome;
+        float saldo;
+        System.out.print("Por favor, digite seu nome: ");
+        nome = teclado.next();
+        System.out.print("Digite sua agência: ");
+        agencia = teclado.next();
+        System.out.print("Digite seu número de conta: ");
+        numero = teclado.nextInt();
+        System.out.println("Digite seu saldo: ");
+        saldo = teclado.nextFloat();
+        System.out.print("Olá "+nome+", obrigado por criar uma conta em nosso banco, sua agência é "+agencia+"," +
+                " conta "+numero+" e seu saldo "+saldo+" já está disponível para saque");
+    }
 }
